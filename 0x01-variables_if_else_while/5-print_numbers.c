04f23599467a187c6bf13db3358986c65ebe1150
youFness <younsssaggo9999@gmail.com>
2023-03-18 14:02:29 -0700

    project

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description:'print numbers'
+*Return: always 0
+*/
+int main(void)
+{
+	int n;
+
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+
+printf("\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description:'print comb numbers'
+*Return: always 0
+*/
+int main(void)
+{
+	int n;
+	
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+if (n == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+	return (0);
+}
